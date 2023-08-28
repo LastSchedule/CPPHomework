d27518a5bf25f35c72e45b4dd485dd01e67705ce
石高炯 <shigaojiong@kingsoft.com>
2023-08-28 09:44:39 +0800

    feat: 增加边界插入时边界情况判断

--- Octree/Octree.h
+++ Octree/Octree.h
@@ -11,17 +11,16 @@ class Octree {
 public:
 	Octree(const Vec3& lowerLeftBack, const Vec3& highRightFront)
 		: lowerLeftBack(lowerLeftBack), highRightFront(highRightFront), data(nullptr) {
-		// Initially, there are no children
-		for (int i = 0; i < 8; ++i)
-			children[i] = nullptr;
+		for (auto child : children)
+			child = nullptr;
 	}
 
 	Octree(const Octree& copy)
 		: lowerLeftBack(copy.lowerLeftBack), highRightFront(copy.highRightFront), data(copy.data) {}
 
 	~Octree() {
-		for (int i = 0; i < 8; ++i)
-			delete children[i];
+		for (auto child : children)
+			delete child;
 	}
 
 	int getOctantContainingPoint(const Vec3& point) const {
@@ -38,11 +37,9 @@ public:
 	}
 
 	void insert(OctreePoint* point) {
-		//if (point->getPosition().x< lowerLeftBack.x|| point->getPosition().x > highRightFront.x || point->getPosition().y < lowerLeftBack.y || point->getPosition().y > highRightFront.y || point->getPosition().z < lowerLeftBack.z || point->getPosition().z > highRightFront.z) {
-		//	cout << "insert failed! because the point is out of range"<< point->getPosition().x << point->getPosition().y << point->getPosition().z<< endl;
-		//	cout << lowerLeftBack.x << lowerLeftBack.y << lowerLeftBack.z << endl;
-		//	return;
-		//}
+		if (point->getPosition().x< lowerLeftBack.x|| point->getPosition().x > highRightFront.x || point->getPosition().y < lowerLeftBack.y || point->getPosition().y > highRightFront.y || point->getPosition().z < lowerLeftBack.z || point->getPosition().z > highRightFront.z) {
+			return;
+		}
 
 		if (isLeafNode()) {
 			// 当前节点为叶子节点且未存入信息则存入该点
@@ -56,19 +53,18 @@ public:
 					cout << "insert failed! because the point is already exist" << endl;
 				}
 				else {
-					// cout << lowerLeftBack.x << " " << lowerLeftBack.y << " " << lowerLeftBack.z << " " << endl;
 					OctreePoint* oldPoint = data;
 					data = nullptr;
 					Vec3 origin = (highRightFront + lowerLeftBack) / 2;
 					Vec3 offsetX = Vec3(origin.x - lowerLeftBack.x,0, 0 );
 					Vec3 offsetY = Vec3(0, origin.y - lowerLeftBack.y, 0 );
 					Vec3 offsetZ = Vec3(0, 0, origin.z - lowerLeftBack.z);
-					Vec3 Vec3Zero = Vec3(0, 0, 0);
+					Vec3 vec3Zero = Vec3(0, 0, 0);
 					for (int i = 0; i < 8; ++i) {
-						Vec3 newOffset = Vec3Zero;
-						newOffset += i & 4 ? offsetX : Vec3Zero;
-						newOffset += i & 2 ? offsetY : Vec3Zero;
-						newOffset += i & 1 ? offsetZ : Vec3Zero;
+						Vec3 newOffset = vec3Zero;
+						newOffset += i & 4 ? offsetX : vec3Zero;
+						newOffset += i & 2 ? offsetY : vec3Zero;
+						newOffset += i & 1 ? offsetZ : vec3Zero;
 						children[i] = new Octree(lowerLeftBack + newOffset, origin + newOffset);
 					}
 					children[getOctantContainingPoint(oldPoint->getPosition())]->insert(oldPoint);
@@ -93,14 +89,14 @@ public:
 			}
 		}
 		else {
-			for (int i = 0; i < 8; ++i) {
-				Vec3 cmax = children[i]->highRightFront;
-				Vec3 cmin = children[i]->lowerLeftBack;
+			for (auto child : children) {
+				Vec3 cmax = child->highRightFront;
+				Vec3 cmin = child->lowerLeftBack;
 
 				if (cmax.x < bmin.x || cmax.y < bmin.y || cmax.z < bmin.z) continue;
 				if (cmin.x > bmax.x || cmin.y > bmax.y || cmin.z > bmax.z) continue;
 
-				children[i]->find(bmin, bmax, results);
+				child->find(bmin, bmax, results);
 			}
 		}
 	}
